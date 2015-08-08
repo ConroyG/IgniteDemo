274a00c652378ad0dbbd4be2cef6aa6801907b21
Conroy Griffiths <conroyg@stormlight.co.nz>
2015-08-08 16:57:16 +1200

    Added Compile Typescript task

--- IgniteTimetableDemo/gulpfile.js
+++ IgniteTimetableDemo/gulpfile.js
@@ -4,7 +4,31 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var tsc = require('gulp-typescript');
 
-gulp.task('default', function () {
-    // place code for your default task here
+var config = {
+    allTypeScript: './wwwroot/js/**/*.js',
+    libraryTypeScriptDefinitions: './typings/**/*.ts',
+    appTypeScriptReferences: './typings/tsd.d.ts',
+    tsOutputPath: './wwwroot/js',
+};
+
+gulp.task('compile-ts', function () {
+    var sourceTsFiles = [config.allTypeScript,                
+                         config.libraryTypeScriptDefinitions, 
+                         config.appTypeScriptReferences];     
+
+    var tsResult = gulp.src(sourceTsFiles)
+                       .pipe(sourcemaps.init())
+                       .pipe(tsc({
+                           target: 'ES5',
+                           declarationFiles: false,
+                           noExternalResolve: true
+                       }));
+
+    tsResult.dts.pipe(gulp.dest(config.tsOutputPath));
+    return tsResult.js
+                    .pipe(sourcemaps.write('.'))
+                    .pipe(gulp.dest(config.tsOutputPath));
 });
